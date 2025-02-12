11c643f6b3c2a30af405d1e4fe50ee8dfc27f5b9
dxt <raf.giebultowski@gmail.com>
2025-02-12 15:55:14 +0100

    Reservation controller

--- ReservationApp/Controllers/ReservationsController.cs
+++ ReservationApp/Controllers/ReservationsController.cs
@@ -1,7 +1,51 @@
-﻿namespace ReservationApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReservationApp.Data;
+using ReservationApp.Models;
+
+namespace ReservationApp.Controllers
 {
-    public class ReservationsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public ReservationsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Reservations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reservation>>> GetReservations()
+        {
+            var reservations = await context.Reservations.Include(room => room.RoomId).ToListAsync();
+            if (reservations == null)
+                return NotFound();
+
+            return Ok(reservations);
+        }
+
+        // GET: api/Reservations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reservation>> GetReservation(int id)
+        {
+            var reservation = await context.Reservations.Include(room => room.RoomId).FirstOrDefaultAsync(reservation => reservation.Id == id);
+            if (reservation == null)
+                return NotFound();
+
+            return Ok(reservation);
+        }
+
+        // POST: api/Reservations
+        [HttpPost]
+        public async Task<ActionResult<Reservation>> PostReservation(Reservation reservation)
+        {
+            //TODO: Colision check??? Post new reservation
+
+            return CreatedAtAction();
+        }
 
     }
 }
